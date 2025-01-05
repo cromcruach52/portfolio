7e06ab8f2272e5e199a33951a24cc3f4990d1270
Nicko <Nicko@LAPTOP-ANCPA317>
2025-01-05 15:35:56 +0800

    modified cv link

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 from config import PROFILE, PROJECTS, NOTEBOOKS, PAPERS, ACHIEVEMENTS, ACHIEVEMENT_DESCRIPTION, CERTIFICATIONS
 
 app = Flask(__name__)
